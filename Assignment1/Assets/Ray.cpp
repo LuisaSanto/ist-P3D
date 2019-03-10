43af2432114a72ff4e8f17ddc7e406c70d5d17b2
VascoNunes <vas.nunes@gmail.com>
2019-03-10 19:00:36 +0000

    Added a ray class and upgraded the makefile

---
+++ Assignment1/Assets/Ray.cpp
@@ -0,0 +1,6 @@
+#include "Ray.h"
+
+Ray::Ray(Point origin, Point direction) {
+	_origin = origin;
+	_direction = direction;
+}
---
+++ Assignment1/Assets/Ray.h
@@ -0,0 +1,24 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "Point.h"
+
+using namespace std;
+
+//Class to represent a Ray
+class Ray {
+	private:
+		Point _origin;
+		Point _direction;
+
+	public:
+		//Constructor
+		Ray(Point origin, Point direction);
+
+		//Getters
+
+		Point getOrigin() { return _origin; }
+		Point getDirection() { return _direction; }
+};
+
+#endif
