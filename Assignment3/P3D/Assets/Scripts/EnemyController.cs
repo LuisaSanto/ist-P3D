b70112cc95e188d03502425059e397972b7bfd13
VascoNunes <vas.nunes@gmail.com>
2019-05-11 21:29:04 +0100

    Remove Unecessary Files. Added Enemy AI that goes around the track
    Removed unecessary files. Added Enemy AI that goes around the track. Start screen, score and minimap still need to be miplemented

---
+++ Assignment3/P3D/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[System.Serializable]
+public class EnemyScript : System.Object
+{
+    public WheelCollider leftWheel;
+    public GameObject leftWheelMesh;
+    public WheelCollider rightWheel;
+    public GameObject rightWeelMesh;
+    public bool motor;
+    public bool steering;
+    public bool reverseTurn;
+}
+
+public class EnemyController : MonoBehaviour
+{
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+    public List<Dot_Truck> truck_Infos;
+    private Rigidbody rb;
+
+    public float MotorValue;
+    public float steeringValue;
+    public float breakValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void VisualizeWheel(Dot_Truck wheelPair)
+    {
+        Quaternion rot;
+        Vector3 pos;
+        wheelPair.leftWheel.GetWorldPose(out pos, out rot);
+        wheelPair.leftWheelMesh.transform.position = pos;
+        wheelPair.leftWheelMesh.transform.rotation = rot;
+        wheelPair.rightWheel.GetWorldPose(out pos, out rot);
+        wheelPair.rightWheelMesh.transform.position = pos;
+        wheelPair.rightWheelMesh.transform.rotation = rot;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float motor = maxMotorTorque * MotorValue;
+        float steering = maxSteeringAngle * steeringValue;
+        float brakeTorque = Mathf.Abs(breakValue);
+
+        if (brakeTorque > 0.001)
+        {
+            brakeTorque = maxMotorTorque;
+            motor = 0;
+            rb.drag = 2.0f;
+        } else
+        {
+            brakeTorque = 0;
+            rb.drag = 0;
+        }
+
+        foreach (Dot_Truck truck_Info in truck_Infos)
+        {
+            if (truck_Info.steering == true)
+            {
+                truck_Info.leftWheel.steerAngle = truck_Info.rightWheel.steerAngle = ((truck_Info.reverseTurn) ? 1 : 1) * steering;
+            }
+
+            if (truck_Info.motor == true)
+            {
+                truck_Info.leftWheel.motorTorque = motor;
+                truck_Info.rightWheel.motorTorque = motor;
+            }
+            truck_Info.leftWheel.brakeTorque = brakeTorque;
+            truck_Info.rightWheel.brakeTorque = brakeTorque;
+
+            VisualizeWheel(truck_Info);
+        }
+    }
+
+
+}
---
+++ Assignment3/P3D/Assets/Scripts/EnemyMovements.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovements : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public string objectName;
+    private GameObject _object;
+    public bool aux;
+    private GameObject enemy;
+    private GameObject gc;
+
+
+    public Action ac = new Action();
+
+    public enum Action
+    {
+        accelerate,
+        brake,
+        turnLeft,
+        turnRight,
+        lowSpeed,
+        ultraLowSpeed
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GameObject.Find("Enemy");
+        agent = enemy.GetComponent<NavMeshAgent>();
+        agent.enabled = true;
+
+        if (aux)
+        {
+            _object = GameObject.Find(objectName);
+        }
+
+        gc = GameObject.Find("GameController");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            print("Ola\n");
+            print(ac);
+            switch (ac)
+            {   
+         
+                case Action.accelerate:
+                    if (aux)
+                    {
+                        agent.SetDestination(_object.transform.position);
+                        other.transform.LookAt(other.GetComponent<NavMeshAgent>().destination);
+                        other.GetComponent<EnemyController>().maxMotorTorque = 700;
+                        other.GetComponent<EnemyController>().MotorValue = 0.5f;
+                        other.GetComponent<EnemyController>().steeringValue = 0;
+                        other.GetComponent<EnemyController>().breakValue = 0;
+                        break;
+                    }
+                    else
+                    {
+                        other.GetComponent<EnemyController>().maxMotorTorque = 600;
+                        other.GetComponent<EnemyController>().MotorValue = 0.5f;
+                        other.GetComponent<EnemyController>().steeringValue = 0;
+                        other.GetComponent<EnemyController>().breakValue = 0;
+                        break;
+                    }
+
+                case Action.brake:
+                    if (aux && gc.GetComponent<GameController>().numLapOpponent <= 0)
+                    {
+                        agent.SetDestination(_object.transform.position);
+                        other.transform.LookAt(other.GetComponent<NavMeshAgent>().destination);
+                        other.GetComponent<EnemyController>().maxMotorTorque = 600;
+                        other.GetComponent<EnemyController>().MotorValue = 0.5f;
+                        other.GetComponent<EnemyController>().steeringValue = 0;
+                        other.GetComponent<EnemyController>().breakValue = 1;
+                        break;
+                    }
+                    else if (aux && gc.GetComponent<GameController>().numLapOpponent > 0)
+                    {
+                        agent.SetDestination(_object.transform.position);
+                        other.transform.LookAt(other.GetComponent<NavMeshAgent>().destination);
+                        other.GetComponent<EnemyController>().maxMotorTorque = 600;
+                        other.GetComponent<EnemyController>().MotorValue = 0.5f;
+                        other.GetComponent<EnemyController>().steeringValue = 0;
+                        other.GetComponent<EnemyController>().breakValue = 2.0f;
+                        break;
+                    }
+                    else
+                    {
+                        other.GetComponent<EnemyController>().maxMotorTorque = 600;
+                        other.GetComponent<EnemyController>().MotorValue = 0;
+                        other.GetComponent<EnemyController>().steeringValue = 0;
+                        other.GetComponent<EnemyController>().breakValue = 0;
+                        break;
+                    }
+
+                case Action.turnLeft:
+                    if (aux)
+                    {
+                        agent.SetDestination(_object.transform.position);
+                        other.GetComponent<EnemyController>().maxMotorTorque = 600;
+                        other.GetComponent<EnemyController>().MotorValue = 1;
+                        other.GetComponent<EnemyController>().steeringValue = -1;
+                        other.GetComponent<EnemyController>().breakValue = 0;
+                        break;
+                    }
+                    else
+                    {
+                        other.GetComponent<EnemyController>().maxMotorTorque = 600;
+                        other.GetComponent<EnemyController>().MotorValue = 1;
+                        other.GetComponent<EnemyController>().steeringValue = -1;
+                        other.GetComponent<EnemyController>().breakValue = 0;
+                        break;
+                    }
+
+                case Action.turnRight:
+                    if (aux)
+                    {
+                        agent.SetDestination(_object.transform.position);
+                        other.GetComponent<EnemyController>().maxMotorTorque = 600;
+                        other.GetComponent<EnemyController>().MotorValue = 1;
+                        other.GetComponent<EnemyController>().steeringValue = 1;
+                        other.GetComponent<EnemyController>().breakValue = 0;
+                        break;
+                    }
+                    else
+                    {
+                        other.GetComponent<EnemyController>().maxMotorTorque = 600;
+                        other.GetComponent<EnemyController>().MotorValue = 1;
+                        other.GetComponent<EnemyController>().steeringValue = 1;
+                        other.GetComponent<EnemyController>().breakValue = 0;
+                        break;
+                    }
+
+                case Action.lowSpeed:
+                    if (aux)
+                    {
+                        agent.SetDestination(_object.transform.position);
+                        other.transform.LookAt(other.GetComponent<NavMeshAgent>().destination);
+                        other.GetComponent<EnemyController>().maxMotorTorque = 600;
+                        other.GetComponent<EnemyController>().MotorValue = 0.2f;
+                        other.GetComponent<EnemyController>().steeringValue = 0;
+                        other.GetComponent<EnemyController>().breakValue = 0;
+                        break;
+                    }
+                    else
+                    {
+                        other.GetComponent<EnemyController>().maxMotorTorque = 600;
+                        other.GetComponent<EnemyController>().MotorValue = 0.2f;
+                        other.GetComponent<EnemyController>().steeringValue = 0;
+                        other.GetComponent<EnemyController>().breakValue = 0;
+                        break;
+                    }
+
+                case Action.ultraLowSpeed:
+                    if (aux)
+                    {
+                        agent.SetDestination(_object.transform.position);
+                        other.transform.LookAt(other.GetComponent<NavMeshAgent>().destination);
+                        other.GetComponent<EnemyController>().maxMotorTorque = 600;
+                        other.GetComponent<EnemyController>().MotorValue = 0.005f;
+                        other.GetComponent<EnemyController>().steeringValue = 0;
+                        other.GetComponent<EnemyController>().breakValue = 0;
+                        break;
+                    }
+                    else
+                    {
+                        other.GetComponent<EnemyController>().maxMotorTorque = 600;
+                        other.GetComponent<EnemyController>().MotorValue = 0.005f;
+                        other.GetComponent<EnemyController>().steeringValue = 0;
+                        other.GetComponent<EnemyController>().breakValue = 0;
+                        break;
+                    }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
