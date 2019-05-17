6da6c46a9f0662ff851be89c56c7e452ba3cd5fb
VascoNunes <vas.nunes@gmail.com>
2019-05-17 12:02:59 +0100

    Added snow (particle effect)

---
+++ Assignment3/P3D/Assets/Scripts/SnowController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowController : MonoBehaviour
+{
+    private ParticleSystem system;
+    private GameObject gc;
+    private bool isEnabled;
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+        gc = GameObject.Find("Snow");
+        system = gc.GetComponent<ParticleSystem>();
+        print(system);
+        system.Stop();
+        isEnabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        toogleOnorOff();
+    }
+
+    private void toogleOnorOff()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            isEnabled = !isEnabled;
+            system.Play();
+            if (isEnabled)
+                system.Play();
+            else
+                system.Stop();
+        }
+    }
+
+}
